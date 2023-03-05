29ec24289401db91e710e856d96358a0e7bfee36
quantasium <120335722+quantasium@users.noreply.github.com>
2023-03-05 20:40:49 +0000

    v0.4.2

--- bin.js
+++ bin.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 import fs from 'node:fs';
 import path from 'node:path';
+
 import * as prompt from '@clack/prompts';
 import format from 'kleur';
+
+import * as util from './utils';
 
 const { version } = JSON.parse(
   fs.readFileSync(new URL('package.json', import.meta.url), 'utf-8')
@@ -39,52 +42,120 @@ if (fs.existsSync(cwd)) {
 
 const options = await prompt.group(
   {
-    types: () =>
+    framework: () =>
       prompt.select({
-        message: 'Which language?',
+        message: 'Which framework?',
         options: [
           {
             label: `${format.yellow('Vanilla')}`,
-            value: 0,
+            value: 'va',
           },
           {
             label: `${format.white('Svelte')}`,
-            value: 1,
+            value: 'sv',
           },
           {
             label: `${format.green('Vue')}`,
-            value: 2,
+            value: 'vu',
           },
           {
             label: `${format.blue('React')}`,
-            value: 3,
+            value: 're',
           },
           {
             label: `${format.magenta('Preact')}`,
-            value: 4,
+            value: 'pr',
           },
           {
             label: `${format.red('Lit')}`,
-            value: 5,
+            value: 'li',
           },
         ],
       }),
 
-    features: () =>
-      prompt.multiselect({
-        message: 'Select additional options (use arrow keys/space bar)',
-        required: false,
+    lang: () =>
+      prompt.select({
+        message: 'Which language?',
         options: [
           {
-            value: 'eslint',
-            label: 'Add ESLint for code linting',
+            label: `${format.yellow('JavaScript')}`,
+            value: 'js',
           },
           {
-            value: 'prettier',
-            label: 'Add Prettier for code formatting',
+            label: `${format.blue('TypeScript')}`,
+            value: 'ts',
           },
         ],
       }),
   },
   { onCancel: () => process.exit(1) }
 );
+
+prompt.outro(`Your project is ready!`);
+
+if (cwd && !fs.existsSync(cwd)) {
+  fs.mkdirSync(cwd);
+} else {
+  fs.readdirSync(cwd, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      fs.unlinkSync(path.join(cwd, file), err => {
+        if (err) throw err;
+      });
+    }
+  });
+}
+
+fs.writeFileSync(join(cwd, 'p'));
+
+switch (options.framework) {
+  case 'va':
+    switch (options.lang) {
+      case 'js':
+        break;
+      case 'ts':
+        break;
+    }
+    break;
+  case 'sv':
+    switch (options.lang) {
+      case 'js':
+        break;
+      case 'ts':
+        break;
+    }
+    break;
+  case 'vu':
+    switch (options.lang) {
+      case 'js':
+        break;
+      case 'ts':
+        break;
+    }
+    break;
+  case 're':
+    switch (options.lang) {
+      case 'js':
+        break;
+      case 'ts':
+        break;
+    }
+    break;
+  case 'pr':
+    switch (options.lang) {
+      case 'js':
+        break;
+      case 'ts':
+        break;
+    }
+    break;
+  case 'li':
+    switch (options.lang) {
+      case 'js':
+        break;
+      case 'ts':
+        break;
+    }
+    break;
+}
