66b17b59edc96d1b3010ef1c69bc194ade51005f
SpideyZac <66804504+SpideyZac@users.noreply.github.com>
2023-03-05 17:37:30 -0800

    more examples and some fixes

--- src/bin.ts
+++ src/bin.ts
@@ -5,19 +5,19 @@ import {
   rmSync,
   mkdirSync,
   createReadStream,
-} from 'node:fs';
-import { text, isCancel, confirm, group, select, outro } from '@clack/prompts';
-import { color } from 'console-log-colors';
-import unzipper from 'unzipper';
+} from "node:fs";
+import { text, isCancel, confirm, group, select, outro } from "@clack/prompts";
+import { color } from "console-log-colors";
+import unzipper from "unzipper";
 
-let cwd = process.argv[2] || '.';
+let cwd = process.argv[2] || ".";
 
 console.log(`${color.grey(`create-chroma`)}`);
 
-if (cwd === '.') {
+if (cwd === ".") {
   const dir = await text({
-    message: 'Where should we create your project?',
-    placeholder: '  (hit Enter to use current directory)',
+    message: "Where should we create your project?",
+    placeholder: "  (hit Enter to use current directory)",
   });
 
   if (isCancel(dir)) process.exit(1);
@@ -30,7 +30,7 @@ if (cwd === '.') {
 if (existsSync(cwd)) {
   if (readdirSync(cwd).length > 0) {
     const force = await confirm({
-      message: 'Directory not empty. Continue?',
+      message: "Directory not empty. Continue?",
       initialValue: false,
     });
 
@@ -44,44 +44,54 @@ const options = await group(
   {
     framework: () =>
       select({
-        message: 'Which framework?',
+        message: "Which framework?",
         options: [
           {
-            label: `${color.yellow('Vanilla')}`,
+            label: `${color.yellow("Vanilla")}`,
             // @ts-ignore
-            value: 'va',
+            value: "vanilla",
           },
           {
-            label: `${color.red('Svelte')}`,
+            label: `${color.red("Svelte")}`,
             // @ts-ignore
-            value: 'sv',
+            value: "svelte",
           },
           {
-            label: `${color.green('Vue')}`,
+            label: `${color.green("Vue")}`,
             // @ts-ignore
-            value: 'vu',
+            value: "vue",
           },
           {
-            label: `${color.blue('React')}`,
+            label: `${color.blue("React")}`,
             // @ts-ignore
-            value: 're',
+            value: "react",
+          },
+          {
+            label: `${color.white("Static")}`,
+            // @ts-ignore
+            value: "static",
           },
         ],
       }),
 
     lang: () =>
       select({
-        message: 'Which language?',
+        message: "Which language?",
         options: [
           {
-            label: `${color.yellow('JavaScript')}`,
+            label: `${color.yellow("JavaScript")}`,
             // @ts-ignore
-            value: 'js',
+            value: "js",
           },
           {
-            label: `${color.blue('TypeScript')}`,
+            label: `${color.blue("TypeScript")}`,
             // @ts-ignore
-            value: 'ts',
+            value: "ts",
+          },
+          {
+            label: `${color.blue("HTML (Choose if You Used Static)")}`,
+            // @ts-ignore
+            value: "html",
           },
         ],
       }),
@@ -92,10 +102,22 @@ const options = await group(
 if (existsSync(cwd)) rmSync(cwd, { recursive: true, force: true });
 mkdirSync(cwd);
 
-createReadStream(`./assets/${options.framework}.${options.lang}.zip`).pipe(
-  unzipper.Extract({ path: cwd })
-);
+if (options.framework === "static") {
+  createReadStream("./assets/static.html.zip").pipe(
+    unzipper.Extract({ path: cwd })
+  );
+} else {
+  createReadStream(`./assets/${options.framework}.${options.lang}.zip`).pipe(
+    unzipper.Extract({ path: cwd })
+  );
+}
 
-outro(
-  `Your project is ready!\n\tNext Steps:\n\nnpm install\nnpm run dev\n\nIf you need help, see the docs (https://chromajs.github.io).`
-);
+if (options.framework === "static") {
+  outro(
+    `Your project is ready!\n\tNext Steps:\n\ncd ${cwd}\nnpm install\nnpx chroma-html-compiler -s ./src -o ./out\n\nIf you need help, see the docs (https://chromajs.github.io).`
+  );
+} else {
+  outro(
+    `Your project is ready!\n\tNext Steps:\n\ncd ${cwd}\nnpm install\nnpm run dev\n\nIf you need help, see the docs (https://chromajs.github.io).`
+  );
+}
